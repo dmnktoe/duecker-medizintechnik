3816ef4bd44e77c6e78e1c0acaa94e403c3483a3
Domenik Töfflinger <domenikt96@yahoo.de>
2023-07-08 23:26:59 +0200

    feat: create container component

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,15 @@
+import clsx from 'clsx';
+
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={clsx('mx-auto max-w-[90rem] px-8', className)}>
+      {children}
+    </div>
+  );
+};
