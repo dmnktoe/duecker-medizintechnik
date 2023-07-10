ee16cced192fc967e170c3b9d66eec1f3e366d0d
Domenik Töfflinger <domenikt96@yahoo.de>
2023-07-10 23:04:56 +0200

    style: update intro jumbotron

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -1,43 +1,36 @@
 import * as React from 'react';
 
 import { Container } from '@/components/layout/Container';
+import ButtonLink from '@/components/links/ButtonLink';
 import NextImage from '@/components/NextImage';
 
 export const Intro = () => {
   return (
-    <div className='relative isolate px-6 pt-14 lg:px-8'>
-      <div
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-        aria-hidden='true'
-      >
-        <div
-          className='from-primary-800 to-primary-300 relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
-      <Container className='py-32 sm:py-48 lg:py-56'>
-        <div className='flex'>
-          <IntroText />
-          <IntroText />
+    <>
+      <BackgroundBlurTop />
+      <Container>
+        <div className='my-auto flex h-[80vh] min-h-[60rem] items-center gap-x-48'>
+          <div className='w-full'>
+            <IntroText />
+          </div>
+          <div className='hidden w-full md:block'>
+            <div className='absolute right-0 top-0 h-full max-h-[60rem] w-1/2 overflow-y-hidden object-cover'>
+              <NextImage
+                alt='hero'
+                src='/images/hero_bg.jpg'
+                useSkeleton={true}
+                blurDataURL='/images/hero_bg.jpg'
+                width={1800}
+                height={1800}
+                className='h-full w-full overflow-hidden rounded-bl-[50vw]'
+              />
+            </div>
+          </div>
         </div>
         <CustomerLogos />
       </Container>
-      <div
-        className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
-        aria-hidden='true'
-      >
-        <div
-          className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
-    </div>
+      <BackgroundBlurBottom />
+    </>
   );
 };
 
@@ -46,17 +39,17 @@ const IntroText = () => {
     <>
       <div>
         <div className='hidden sm:mb-8 sm:flex'>
-          <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+          <div className='hover:ring-primary-900/20 relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10'>
             Announcing our next round of funding.{' '}
-            <a href='#' className='font-semibold text-indigo-600'>
+            <a href='#' className='text-primary-500 font-semibold'>
               <span className='absolute inset-0' aria-hidden='true' />
               Read more <span aria-hidden='true'>&rarr;</span>
             </a>
           </div>
         </div>
         <div className='text-left'>
-          <h1 className='text-3xl font-medium tracking-tight text-gray-900 sm:text-5xl'>
-            Medizintechnik-Lösungen von Dücker Medizintechnik
+          <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+            OP-Lösungen und Sterilisierungen von Dücker Medizintechnik
           </h1>
           <p className='mt-6 text-lg leading-8 text-gray-600'>
             Entdecken Sie unser breites Spektrum an Produkten und
@@ -64,13 +57,13 @@ const IntroText = () => {
             Sie, wie wir Ihnen dabei helfen, eine optimale Versorgung im
             medizinischen Kontext zu gewährleisten.
           </p>
-          <div className='mt-10 flex items-center justify-center gap-x-6'>
-            <a
+          <div className='mt-10 flex items-center justify-start gap-x-6'>
+            <ButtonLink
               href='#'
-              className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              className='px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
             >
               Get started
-            </a>
+            </ButtonLink>
             <a
               href='#'
               className='text-sm font-semibold leading-6 text-gray-900'
@@ -86,67 +79,102 @@ const IntroText = () => {
 
 const CustomerLogos = () => {
   return (
-    <div className='mt-20 grid grid-cols-2 gap-8 sm:grid-cols-6 lg:grid-cols-6'>
-      <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
+    <div className='grid grid-cols-2 justify-around gap-8 text-neutral-600 md:grid-cols-3 lg:grid-cols-6'>
+      <div className='flex'>
         <NextImage
-          useSkeleton
-          className='h-10'
+          useSkeleton={true}
           src='/svg/bissinger.svg'
+          blurDataURL='/svg/bissinger.svg'
           width='100'
           height='100'
           alt='Icon'
+          className='text-neutral-600'
         />
       </div>
-      <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
+      <div className='self-center'>
         <NextImage
           useSkeleton
-          className='h-10'
           src='/svg/eberle.svg'
+          blurDataURL='/svg/eberle.svg'
           width='100'
           height='100'
           alt='Icon'
         />
       </div>
-      <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
+      <div className='self-center'>
         <NextImage
           useSkeleton
-          className='h-10'
           src='/svg/hupfer.svg'
+          blurDataURL='/svg/hupfer.svg'
           width='100'
           height='100'
           alt='Icon'
         />
       </div>
-      <div className='col-span-1 flex justify-center md:col-span-2 lg:col-span-1'>
+      <div className='self-center'>
         <NextImage
           useSkeleton
-          className='h-10'
           src='/svg/keysurgical.svg'
+          blurDataURL='/svg/keysurgical.svg'
           width='100'
           height='100'
           alt='Icon'
         />
       </div>
-      <div className='col-span-2 flex justify-center md:col-span-2 lg:col-span-1'>
+      <div className='self-center'>
         <NextImage
           useSkeleton
-          className='h-10'
           src='/svg/medicon.svg'
+          blurDataURL='/svg/medicon.svg'
           width='100'
           height='100'
           alt='Icon'
         />
       </div>
-      <div className='col-span-2 flex justify-center md:col-span-2 lg:col-span-1'>
+      <div className='self-center'>
         <NextImage
           useSkeleton
-          className='h-10'
           src='/svg/nouvag.svg'
+          blurDataURL='/svg/nouvag.svg'
           width='100'
           height='100'
           alt='Icon'
         />
       </div>
+    </div>
+  );
+};
+
+const BackgroundBlurTop = () => {
+  return (
+    <div
+      className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+      aria-hidden='true'
+    >
+      <div
+        className='from-primary-800 to-primary-300 relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+        style={{
+          clipPath:
+            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }}
+      />
+    </div>
+  );
+};
+
+const BackgroundBlurBottom = () => {
+  return (
+    <div
+      className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+      aria-hidden='true'
+    >
+      <div
+        className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
+        style={{
+          clipPath:
+            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+        }}
+      />
     </div>
   );
 };
