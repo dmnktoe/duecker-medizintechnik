1c0d944fe6eda786cc8b1c2a8347017e61f55ef3
Domenik Töfflinger <domenikt96@yahoo.de>
2023-07-10 22:56:56 +0200

    feat: add features sticky scroll section

---
+++ src/components/sections/features/title.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+import { useInView } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+
+import { useFeatureStore } from './store';
+
+type Props = {
+  children: React.ReactNode;
+  id: string;
+};
+
+export const FeatureTitle = ({ children, id }: Props) => {
+  const ref = useRef<HTMLParagraphElement>(null);
+  const isInView = useInView(ref, {
+    margin: '-50% 0px -50% 0px',
+  });
+  const setInViewFeature = useFeatureStore((state) => state.setInViewFeature);
+  const inViewFeature = useFeatureStore((state) => state.inViewFeature);
+
+  useEffect(() => {
+    if (isInView) setInViewFeature(id);
+    if (!isInView && inViewFeature === id) setInViewFeature(null);
+  }, [isInView, id, setInViewFeature, inViewFeature]);
+
+  return (
+    <p
+      ref={ref}
+      className={clsx(
+        'feature-title font-heading py-16 text-3xl font-bold transition-colors sm:text-5xl',
+        isInView ? 'text-black' : 'text-gray-300'
+      )}
+    >
+      {children}
+    </p>
+  );
+};
