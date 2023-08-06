1a26289c01b9c16df78136cf2e9a9562e7e5d395
Domenik Töfflinger <domenikt96@yahoo.de>
2023-08-06 21:40:01 +0200

    style: update nextimage and add card

---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import NextImage from '@/components/NextImage';
+
+interface CardProps {
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+}
+
+const Card: React.FC<CardProps> = ({ title, description, category, image }) => {
+  return (
+    <div className='w-full md:mr-10 md:max-w-lg md:flex-shrink-0'>
+      <a className='group block' href='#'>
+        <div className='rounded-4xl relative mb-6 overflow-hidden'>
+          <NextImage
+            className='block h-72 w-full object-cover'
+            src={image}
+            alt=''
+            layout='fill'
+          />
+          <div className='absolute left-0 top-0 h-full w-full transition duration-200 group-hover:bg-black group-hover:bg-opacity-10' />
+        </div>
+        <div className='max-w-xs sm:max-w-md'>
+          <div className='mb-3'>
+            <span className='border-1.5 mr-6 inline-block rounded-full border-black px-3 py-2 text-sm leading-none text-black transition duration-200 group-hover:bg-black group-hover:text-white'>
+              {category}
+            </span>
+            <span className='text-coolGray-600 inline-block text-sm font-medium'>
+              9 min read
+            </span>
+          </div>
+          <h4 className='mb-6 text-3xl tracking-tight sm:text-4xl'>{title}</h4>
+          <p className='max-w-sm'>{description}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default Card;
