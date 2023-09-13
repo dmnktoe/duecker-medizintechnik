07250923a1c7acb7d87612f4295228536ea186ea
Domenik Töfflinger <domenikt96@yahoo.de>
2023-09-14 00:54:20 +0200

    chore: add new translation files

--- @types/resources.ts
+++ @types/resources.ts
@@ -1,17 +1,25 @@
+import aboutUs from '~/locales/de/aboutUs.json';
 import common from '~/locales/de/common.json';
 import contact from '~/locales/de/contact.json';
+import downloads from '~/locales/de/downloads.json';
 import home from '~/locales/de/home.json';
 import imprint from '~/locales/de/imprint.json';
+import news from '~/locales/de/news.json';
 import notFound from '~/locales/de/notFound.json';
 import privacy from '~/locales/de/privacy.json';
+import production from '~/locales/de/production.json';
 
 const resources = {
+  aboutUs,
   common,
   contact,
+  downloads,
   home,
   imprint,
+  news,
   notFound,
   privacy,
+  production,
 } as const;
 
 export default resources;
