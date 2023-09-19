6ba7873a0ed96c3bd09c8e5bd2cfa6f3a330ec42
Domenik Töfflinger <domenikt96@yahoo.de>
2023-09-19 17:17:50 +0200

    feat: add maintenance page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import { appWithTranslation } from 'next-i18next';
 
 import '@/styles/globals.css';
 
+import MaintenancePage from '@/pages/maintenance';
+
 function Duecker({ Component, pageProps }: AppProps) {
+  if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true') {
+    return <MaintenancePage />;
+  }
   return <Component {...pageProps} />;
 }
 
