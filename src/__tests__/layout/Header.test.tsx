d517ddab5df0aa173eb4f2b2202611fc625f5a88
Domenik Töfflinger <domenikt96@yahoo.de>
2023-09-19 15:57:25 +0200

    test: test header menu buttons

--- src/__tests__/layout/Header.test.tsx
+++ src/__tests__/layout/Header.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { Header } from '@/components/layout/Header';
 
 import initializeI18n from '@/utils/i18n-testing';
 
-describe('Call to action', () => {
+describe('Header', () => {
   beforeEach(async () => {
     await initializeI18n(['common']);
     render(<Header />);
@@ -14,5 +14,20 @@ describe('Call to action', () => {
     expect(
       await screen.findByText('Navigation umschalten'),
     ).toBeInTheDocument();
+  });
+  it('should close and open the navigationMenu with navigationButton', async () => {
+    const button = screen.getByTestId('navigationButton');
+    fireEvent.click(button);
+    const menu = screen.getByTestId('navigationMenu');
+    expect(menu).toHaveClass('translate-x-0 opacity-100');
+  });
+  it('closes hamburger menu on escape key press', () => {
+    const button = screen.getByTestId('navigationButton');
+    fireEvent.click(button); // Open the menu
+    const menu = screen.getByTestId('navigationMenu');
+    expect(menu).toHaveClass('translate-x-0 opacity-100'); // Check if menu is open
+
+    fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' }); // Simulate escape key press
+    expect(menu).toHaveClass('translate-x-2 opacity-0'); // Check if menu is closed
   });
 });
