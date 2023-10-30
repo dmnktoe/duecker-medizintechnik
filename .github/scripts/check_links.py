628af2629ad73d98dda0f598a0a28ee6db79dc3e
Domenik Töfflinger <domenikt96@yahoo.de>
2023-10-30 14:50:59 +0100

    ci: update check_links.py and add `user-agent headers`

--- .github/scripts/check_links.py
+++ .github/scripts/check_links.py
@@ -6,6 +6,11 @@ from bs4 import BeautifulSoup
 # Regex to match external URLs
 url_regex = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
+# User-Agent to be used in requests
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
 # Walk through all .tsx files in the repository
 for root, dirs, files in os.walk("."):
   for file in files:
@@ -15,7 +20,7 @@ for root, dirs, files in os.walk("."):
         urls = url_regex.findall(content)
         for url in urls:
           try:
-            response = requests.get(url, timeout=5)
+            response = requests.get(url, timeout=5, headers=headers)
             response.raise_for_status()
           except (requests.HTTPError, requests.ConnectionError):
             print(f"Broken link: {url} in file: {file}")
