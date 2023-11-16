a0106487f39a90bdf6eaf3386042a9d1aa0a41c0
Domenik Töfflinger <domenikt96@yahoo.de>
2023-11-16 01:49:40 +0100

    feat: add chatwoot widget

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import Chatwoot from '@/components/layout/Chatwoot';
 import GoogleAnalytics from '@/components/layout/GoogleAnalytics';
 import Hotjar from '@/components/layout/Hotjar';
 
 import {
+  chatwootId,
   cookieBotId,
   googleAnalyticsId,
   hotjarId,
@@ -24,6 +26,7 @@ export default function Document() {
             type='text/javascript'
           ></script>
         )}
+        <Chatwoot CHATWOOT_ID={chatwootId} />
         <GoogleAnalytics GA_MEASUREMENT_ID={googleAnalyticsId} />
         <Hotjar HOTJAR_ID={hotjarId} />
         <link
