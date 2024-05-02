28ea7872ea3e158e262203efa957c87bf09a3e38
Domenik Töfflinger <domenikt96@yahoo.de>
2024-05-02 16:40:09 +0200

    refactor: sort and structure `Hero.tsx` code

--- src/components/templates/Hero.tsx
+++ src/components/templates/Hero.tsx
@@ -21,16 +21,6 @@ import heroBg from '/public/images/home/duecker-medizintechnik_home_hero-bg.jpg'
 import heroBg2 from '/public/images/home/duecker-medizintechnik_home_hero-bg-2.jpg';
 
 export const Hero = () => {
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
-  const onAutoplayTimeLeft = (s: unknown, time: number, progress: number) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    progressCircle.current.style.setProperty('--progress', 1 - progress);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
   return (
     <section className='hero pb-16 pt-4 md:pb-24 md:pt-12 lg:pb-32'>
       <Container>
@@ -39,61 +29,7 @@ export const Hero = () => {
             <HeroText />
           </div>
           <div className='relative w-full lg:w-6/12'>
-            <div className='relative -top-2 z-20'>
-              <Swiper
-                modules={[Autoplay]}
-                spaceBetween={2}
-                slidesPerView={1}
-                onAutoplayTimeLeft={onAutoplayTimeLeft}
-                autoplay={{
-                  delay: 5000,
-                  disableOnInteraction: false,
-                }}
-                loop={true}
-              >
-                <SwiperSlide>
-                  <Image
-                    alt='OP-Lösungen und Sterilisierungen für den B2B-Betrieb'
-                    src={heroBg}
-                    placeholder='blur'
-                    priority
-                    className='relative block h-full w-full overflow-hidden'
-                    quality={65}
-                  />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Image
-                    alt='OP-Lösungen und Sterilisierungen für den B2B-Betrieb'
-                    src={heroBg2}
-                    placeholder='blur'
-                    priority
-                    className='relative block h-full w-full overflow-hidden'
-                    quality={65}
-                  />
-                </SwiperSlide>
-                <div className='autoplay-progress' slot='container-end'>
-                  <svg viewBox='0 0 48 48' ref={progressCircle}>
-                    <circle cx='24' cy='24' r='20'></circle>
-                  </svg>
-                  <span ref={progressContent}></span>
-                </div>
-              </Swiper>
-            </div>
-            <div className='absolute -bottom-3 -right-3 z-40 w-5/6 bg-primary-500/95 p-6 text-sm tracking-tighter text-white md:-bottom-5 md:-right-5 md:text-base'>
-              Ihr{' '}
-              <UnstyledLink className='underline' href='/unternehmen'>
-                {' '}
-                Ansprechpartner
-              </UnstyledLink>{' '}
-              für Handelsvermittlung von pharmazeutischen Erzeugnissen,
-              medizinischen und orthopädischen Artikeln und Laborbedarf.
-            </div>
-            <div className='absolute -top-2 left-0 z-30 h-12 w-12 bg-white' />
-            <div className='absolute -top-2 left-12 z-30 h-12 w-12 bg-white/30' />
-            <div className='absolute left-0 top-10 z-30 h-12 w-12 bg-white/30' />
-            <div className='absolute bottom-0 right-0 z-30 h-12 w-12 bg-white' />
-            <div className='absolute bottom-0 right-12 z-30 h-12 w-12 bg-white/30' />
-            <div className='absolute bottom-12 right-0 z-30 h-12 w-12 bg-white/30' />
+            <HeroSlider />
           </div>
         </div>
         <div className='mx-auto mb-8 text-center text-xs text-neutral-400 lg:w-1/3'>
@@ -130,6 +66,96 @@ const HeroText = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+const HeroSlider = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s: unknown, time: number, progress: number) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+
+  const Decorators = () => {
+    const positions = [
+      ['-top-2 left-0', 'md:h-12 md:w-12'],
+      ['-top-2 left-6', 'md:left-12 md:h-12 md:w-12 bg-white/30'],
+      ['left-0 top-4', 'md:top-10 md:h-12 md:w-12 bg-white/30'],
+      ['bottom-2 right-0', 'md:h-12 md:w-12'],
+      ['bottom-2 right-6', 'md:right-12 md:h-12 md:w-12 bg-white/30'],
+      ['bottom-8 right-0', 'md:bottom-14 md:h-12 md:w-12 bg-white/30'],
+    ];
+
+    return (
+      <>
+        {positions.map((pos, index) => (
+          <div
+            key={index}
+            className={`absolute z-30 h-6 w-6 bg-white ${pos[0]} ${pos[1]}`}
+          />
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className='relative -top-2 z-20'>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={2}
+          slidesPerView={1}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+        >
+          <SwiperSlide>
+            <Image
+              alt='OP-Lösungen und Sterilisierungen für den B2B-Betrieb'
+              src={heroBg}
+              placeholder='blur'
+              priority
+              className='relative block h-full w-full overflow-hidden'
+              quality={65}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              alt='OP-Lösungen und Sterilisierungen für den B2B-Betrieb'
+              src={heroBg2}
+              placeholder='blur'
+              priority
+              className='relative block h-full w-full overflow-hidden'
+              quality={65}
+            />
+          </SwiperSlide>
+          <div className='autoplay-progress' slot='container-end'>
+            <svg viewBox='0 0 48 48' ref={progressCircle}>
+              <circle cx='24' cy='24' r='20'></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+        </Swiper>
+      </div>
+      <div className='absolute -bottom-3 -right-3 z-40 w-5/6 bg-primary-500/95 p-6 text-sm tracking-tight text-white md:-bottom-4 md:-right-4 md:text-base'>
+        Ihr{' '}
+        <UnstyledLink className='underline' href='/unternehmen'>
+          {' '}
+          Ansprechpartner
+        </UnstyledLink>{' '}
+        für Handelsvermittlung von pharmazeutischen Erzeugnissen, medizinischen
+        und orthopädischen Artikeln und Laborbedarf.
+      </div>
+      <Decorators />
+    </>
   );
 };
 
