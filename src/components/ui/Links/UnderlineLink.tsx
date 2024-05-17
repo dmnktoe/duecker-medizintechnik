3ea32a186aa62b64c9fd5e3ae9c17205f3424df2
Domenik Töfflinger <domenikt96@yahoo.de>
2024-05-17 18:20:52 +0200

    refactor: update link component
    - remove animated underline
    - add `UnderlineLinkProps`
    - remove new tab cursor

--- src/components/ui/Links/UnderlineLink.tsx
+++ src/components/ui/Links/UnderlineLink.tsx
@@ -6,16 +6,24 @@ import UnstyledLink, {
   UnstyledLinkProps,
 } from '@/components/ui/Links/UnstyledLink';
 
-const UnderlineLink = React.forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
-  ({ children, className, ...rest }, ref) => {
+type UnderlineLinkProps = UnstyledLinkProps & {
+  underline?: 'always' | 'hover';
+};
+
+const UnderlineLink = React.forwardRef<HTMLAnchorElement, UnderlineLinkProps>(
+  ({ children, className, underline = 'always', ...rest }, ref) => {
+    const underlineClasses = {
+      always: 'underline hover:text-primary-500',
+      hover: 'hover:underline',
+    };
+
     return (
       <UnstyledLink
         ref={ref}
         {...rest}
         className={clsxm(
-          'animated-underline custom-link inline-flex items-center',
-          'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
-          'border-b border-solid border-dark hover:border-black/0',
+          'focus:outline-dashed focus-visible:rounded-none focus-visible:ring-1 focus-visible:ring-primary-500 focus-visible:ring-offset-2',
+          underlineClasses[underline],
           className,
         )}
       >
