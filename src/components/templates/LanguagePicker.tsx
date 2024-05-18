1f9617d0f9c9249893e2ffc5e48bfca1fe50be15
Domenik Töfflinger <domenikt96@yahoo.de>
2024-05-18 11:07:48 +0200

    fix: remove `.then();` in LanguagePicker

--- src/components/templates/LanguagePicker.tsx
+++ src/components/templates/LanguagePicker.tsx
@@ -20,7 +20,7 @@ const LanguagePicker = ({ className }: Props) => {
     const newPath =
       newLocale === 'de' ? currentPath : `/${newLocale}${currentPath}`;
 
-    router.push(newPath, newPath, { locale: false }).then();
+    router.push(newPath, newPath, { locale: false });
   }
 
   return (
