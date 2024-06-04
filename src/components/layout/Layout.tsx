2a2a07cd0e94b1d018628eb1a45e620e4aab8c71
Domenik Töfflinger <domenikt96@yahoo.de>
2024-06-04 20:49:15 +0200

    style: exclude `InfoBar` from page layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 
-import { Footer, Header, InfoBar } from '@/components/layout';
+import { Footer, Header } from '@/components/layout';
 import { CallToAction } from '@/components/templates/CallToAction';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <InfoBar />
+      {/*
+        <InfoBar />
+       */}
       <Header />
       {children}
       <CallToAction />
