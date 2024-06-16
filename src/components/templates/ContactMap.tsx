6a10cfaff962c61cf4bf79b4b2850701b6a4c778
Domenik Töfflinger <domenikt96@yahoo.de>
2024-06-16 14:35:31 +0200

    fix: remove async useEffect init hook and make divs block to fix layout shift

--- src/components/templates/ContactMap.tsx
+++ src/components/templates/ContactMap.tsx
@@ -8,48 +8,52 @@ export const ContactMap = () => {
   const { t } = useTranslation('contact');
   return (
     <>
-      <div className='cookieconsent-optin-marketing flex h-[350px] items-center bg-gray-100 align-middle'>
-        <iframe
-          data-cookieblock-src='https://maps.google.de/maps?hl=de&q=Dücker+Medizintechnik%20%20%20%20%20Elfershäuser+Str.+18%20%20%20%20Melsungen&t=ROADMAP&z=12&ie=utf8&iwloc=b&output=embed'
-          data-cookieconsent='marketing'
-          width='100%'
-          height='350'
-          id='google-maps'
-        ></iframe>
+      <div className='cookieconsent-optin-marketing'>
+        <div className='relative block flex h-[350px] items-center bg-gray-100 align-middle'>
+          <iframe
+            data-cookieblock-src='https://maps.google.de/maps?hl=de&q=Dücker+Medizintechnik%20%20%20%20%20Elfershäuser+Str.+18%20%20%20%20Melsungen&t=ROADMAP&z=12&ie=utf8&iwloc=b&output=embed'
+            data-cookieconsent='marketing'
+            width='100%'
+            height='350'
+            id='google-maps'
+          ></iframe>
+        </div>
       </div>
-      <div className='cookieconsent-optout-marketing flex h-[350px] items-center bg-gray-100 align-middle'>
-        <Container>
-          <div className='flex flex-col items-center gap-8 align-middle'>
-            <span
-              className='text-center font-normal text-gray-500 md:text-lg'
-              dangerouslySetInnerHTML={{
-                __html: t('content.mapsBanner.cookieNotice'),
-              }}
-            />
-            <div className='flex w-full flex-wrap justify-center gap-4 sm:w-auto'>
-              <Button
-                variant='primary'
-                size='sm'
-                className='w-full sm:w-56'
-                onClick={() => {
-                  window?.Cookiebot?.submitCustomConsent(false, false, true);
+      <div className='cookieconsent-optout-marketing'>
+        <div className='relative block flex h-[350px] items-center bg-gray-100 align-middle'>
+          <Container>
+            <div className='flex flex-col items-center gap-8 align-middle'>
+              <span
+                className='text-center font-normal text-gray-500 md:text-lg'
+                dangerouslySetInnerHTML={{
+                  __html: t('content.mapsBanner.cookieNotice'),
                 }}
-              >
-                {t('content.mapsBanner.accept')}
-              </Button>
-              <Button
-                variant='outline'
-                size='sm'
-                className='w-full sm:w-56'
-                onClick={() => {
-                  window?.Cookiebot?.show();
-                }}
-              >
-                {t('content.mapsBanner.cookieSettings')}
-              </Button>
+              />
+              <div className='flex w-full flex-wrap justify-center gap-4 sm:w-auto'>
+                <Button
+                  variant='primary'
+                  size='sm'
+                  className='w-full sm:w-56'
+                  onClick={() => {
+                    window?.Cookiebot?.submitCustomConsent(false, false, true);
+                  }}
+                >
+                  {t('content.mapsBanner.accept')}
+                </Button>
+                <Button
+                  variant='outline'
+                  size='sm'
+                  className='w-full sm:w-56'
+                  onClick={() => {
+                    window?.Cookiebot?.show();
+                  }}
+                >
+                  {t('content.mapsBanner.cookieSettings')}
+                </Button>
+              </div>
             </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
       </div>
     </>
   );
