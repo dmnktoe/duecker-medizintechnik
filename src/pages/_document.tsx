c5d1efd67950566228608d4bdf3d0d291c35da8a
Domenik Töfflinger <domenikt96@yahoo.de>
2024-06-16 17:21:54 +0200

    refactor: add strategy='beforeInteractive' cookiebot

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 import GoogleAnalytics from '@/components/helpers/GoogleAnalytics';
 import Hotjar from '@/components/helpers/Hotjar';
@@ -14,12 +15,13 @@ export default function Document() {
   return (
     <Html suppressHydrationWarning>
       <Head>
-        <script
+        <Script
           id='Cookiebot'
-          src='https://consent.cookiebot.com/uc.js'
+          src='https://consent.cookiebot.eu/uc.js'
           data-cbid={cookieBotId}
-          async
           type='text/javascript'
+          strategy='beforeInteractive'
+          async
         />
         <GoogleAnalytics GA_MEASUREMENT_ID={googleAnalyticsId} />
         <Hotjar HOTJAR_ID={hotjarId} />
