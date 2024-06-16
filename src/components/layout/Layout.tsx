f75467bd8760941fc428bbe565f657a5b827742b
Domenik Töfflinger <domenikt96@yahoo.de>
2024-06-16 19:44:03 +0200

    refactor: temp disable cta section

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 
 import { Footer, Header } from '@/components/layout';
-import { CallToAction } from '@/components/templates/CallToAction';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {/*
         <InfoBar />
-       */}
+      */}
       <Header />
       {children}
-      <CallToAction />
+      {/*
+        <CallToAction />
+      */}
       <Footer />
     </>
   );
