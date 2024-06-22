f52503b7eeceebf55ecaa5b6d35046f520d8f030
Domenik Töfflinger <domenikt96@yahoo.de>
2024-06-22 20:52:00 +0200

    fix: get base url NEXT_PUBLIC_VERCEL_URL

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -3,7 +3,7 @@ export const getBaseUrl = () => {
     return process.env.NEXT_PUBLIC_APP_URL;
   }
 
-  if (process.env.VERCEL_URL) {
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
